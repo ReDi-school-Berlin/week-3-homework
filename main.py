92619ae8b5c20ebca4618d1a1eb3d37fee467157
BmanCoder <90142031+BmanCoder@users.noreply.github.com>
2021-09-17 11:59:28 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,55 @@
 ###############################
 ##                            #
-##      WEEK 2 HOMEWORK       #
+##      WEEK 3 HOMEWORK       #
 ##                            #
 ##           \o/              #
 ###############################
 
 
-# Exercise 1.)
-# Create 3 variables:
-# - Your favourite food
-# - Your hobby
-# - The current day
-# Then print out a string that says something like:
-# "Thursday was a good day, I ate pizza and played guitar" (hint: use the + operator)
+# Exercise 1.) ERRORS ENCOUNTERED  
+# Before doing exercises 2 to 4 below, be aware of the errors that you encounter
+# For each exercise below, list down some of the errors you saw (developers usually call this error "bug")
+# What did you do to solve the error?
+# NOTE: The more errors you get familiar with, the better. That means you are adding to what you are learning from the lessons. "Practice makes perfect".
+
+# ------ LIST YOUR ERRORS HERE: ------
+#
+#
+#
+#
+#
+
+# ==================================================================================================================================
+# Exercise 2.) USING CONDITIONAL STATEMENTS (if, else, elif)
+# Situation: Citizens apply for financial benefits from the government whose policy is to grant benefits to government employees for only 2 of his children. 
+# A monthly allowance of 1.000Euro is given for each child if he/she is less than or equal to 20 years old.
+# Write a set of codes using the conditional statements (if, else, elif) that checks the following conditions:
+# If citizen's occupation is a government employee, check if he has kids eligible for benefits, calculate the total benefit amount
+# If citizen's occupation is an entrepreneur, output the message "You make sure you pay your tax!"
+# If citizen has other occupation, output the message "This facility is only for government employees!"
+# At the end, output the message "Total benefit availed: " and total benefit amount
+# (hint: Assign different values to the occupation of the citizen)
 
 # ------ ADD YOUR CODE BELOW: ------
 
 
 
 
-
-# Exercise 2.)
-# Let's do some math! Print the results of the following operations:
-# - 59876 multiplied by 4359
-# - 88659 divided by 5498
-# - the remainder when you divide 867 by 23
-# - 5 raised to the power of 89 (yes, Python can work with very large numbers!:))
-
-# ------ ADD YOUR CODE BELOW: ------
-
-
-
-
-
-# Exercise 3.)
-# Practice VSCode! You will work in it a lot, so it's always good (of course this is something we cannot check here but we trust you:) )
-# - Open a bunch of files
-# - Open different workspaces
-# - Create files and folders within VSCode
-# - Watch this video on getting started with VSCode: https://www.youtube.com/watch?v=S320N3sxinE&list=PLj6YeMhvp2S5UgiQnBfvD7XgOMKs3O_G6&index=1
+# ==================================================================================================================================
+# Exercise 3.) APPLYING THE "for" LOOP 
+# Have you watched Mary Poppins? I haven't, but there is a loooooong word that became popular from that movie.
+# Let's do a bit of coding using the "for" loop
+# Assign this loooooong word "supercalifragilisticexpialidocious" to a string variable
+# For each letter in this looooooong word, print it
+# Count the number of letters
+# Print the number of letters at the end.
 
 # ------ ADD YOUR CODE BELOW: ------
 
 
 
 
-
+# Exercise 4.) APPLYING THE "while" LOOP
 # BONUS EXERCISE
 # You don't have to do this if you don't want, just if you are up to the challenge
 # Also, don't be surprised if it is a bit harder than the normal exercises, you might need to do some research on your own to solve it.
